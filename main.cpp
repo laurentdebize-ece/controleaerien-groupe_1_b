4812555522b6e06e57340c11fa5c56ced7b46558
M5-ux <mathiasneris3@gmail.com>
2022-04-28 20:54:09 +0200

    pcc ok

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
      //Initialisation Cartes Map
      sf::Texture Menu_principal;
 
-     Menu_principal.loadFromFile("../Graphic_Content/Map/Sim_Map.png");
+     Menu_principal.loadFromFile("Graphic_Content/Map/Sim_Map.png");
 
      sf::Sprite Sprite(Menu_principal);
 
@@ -29,8 +29,8 @@
 
          switch (choix) {
                  case 1 :
-                     a.PCC(&f);// juste pour test pcc mais mon pc a planté wsv je veriffie si ça fonctione demain
-                    /* Sprite.setScale(
+                    // a.PCC(&f);// juste pour test pcc mais mon pc a planté wsv je veriffie si ça fonctione demain
+                     Sprite.setScale(
                              targetSize.x / Sprite.getLocalBounds().width,
                              targetSize.y / Sprite.getLocalBounds().height);
 
@@ -38,8 +38,8 @@
                      while (!fin) {
                          sf::Event event{};
                          while (window.pollEvent(event)) {
-                             Plane_Movement(event, window, p, a, Sprite);
-                             //show_airport_on_screen( event, window, Sprite,a, font);
+                            // Plane_Movement(event, window, p, a, Sprite);
+                             show_airport_on_screen( event, window, Sprite,a, font);
 
                              //a.show_airport_on_screen(event, window, Sprite);
                              //f.Flight_manual();
@@ -51,7 +51,7 @@
                              }
                          }
                      }
-                    fin = false;*/
+                    fin = false;
                      //toutes les fonctions du simulateur
                      break;
                  case 2 :
